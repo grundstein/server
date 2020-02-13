aa1b26934fc6f75fb574fcf00174addec94307ed
jascha <jascha@jaeh.at>
2020-02-13 05:13:14 +0100

    format

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,9 +1,13 @@
 import { is } from '@magic/test'
 
-import { runCluster } from '../src/index.mjs'
-
-console.log(runCluster)
+import defaultRunCluster, { runCluster } from '../src/index.mjs'
 
 export default [
   { fn: () => runCluster, expect: is.fn, info: 'runCluster is a function' },
+  { fn: () => defaultRunCluster, expect: is.fn, info: 'runCluster default export is a function' },
+  {
+    fn: is.deep.eq(runCluster, defaultRunCluster),
+    expect: true,
+    info: 'both runCluster exports are equal',
+  },
 ]
