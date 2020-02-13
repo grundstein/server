e2bd2a9ea7866b86081c4a625fb0b00233d1945c
jascha <jascha@jaeh.at>
2020-02-13 05:14:54 +0100

    test: format

--- test/spec.mjs
+++ test/spec.mjs
@@ -5,9 +5,5 @@ import defaultRunCluster, { runCluster } from '../src/index.mjs'
 export default [
   { fn: () => runCluster, expect: is.fn, info: 'runCluster is a function' },
   { fn: () => defaultRunCluster, expect: is.fn, info: 'runCluster default export is a function' },
-  {
-    fn: is.deep.eq(runCluster, defaultRunCluster),
-    expect: true,
-    info: 'both runCluster exports are equal',
-  },
+  { fn: is.deep.eq(runCluster, defaultRunCluster), info: 'runCluster exports are equal' },
 ]
