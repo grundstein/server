59d1d98e904547b2668f30c1c2a2581ac784d8be
jascha <jascha@jaeh.at>
2020-02-14 17:24:07 +0100

    remove local store

--- src/store/lib/getCompressed.mjs
+++
@@ -1,9 +0,0 @@
-import fs from '@magic/fs'
-
-export const getCompressed = async fileName => {
-  try {
-    return await fs.readFile(fileName)
-  } catch (e) {
-    return false
-  }
-}
--- src/store/lib/index.mjs
+++
@@ -1,3 +0,0 @@
-export { getCache } from './getCache.mjs'
-export { getCompressed } from './getCompressed.mjs'
-export { getFileContent } from './getFileContent.mjs'
