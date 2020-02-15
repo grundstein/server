24eca45b9db01f85b2dbb61c0bc9e7903699f7a7
jascha <jascha@jaeh.at>
2020-02-15 18:50:24 +0100

    example/api/v1/route: return correctly, add console.log info

--- example/api/v1/route.mjs
+++ example/api/v1/route.mjs
@@ -1,3 +1,11 @@
-export const route = (req, res) => {
-  console.log('route', req.host, req.pathname)
+export default (req, res) => {
+  console.log('api route called, returning hello world')
+
+  return {
+    code: 200,
+    body: 'hello, world.',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  }
 }
