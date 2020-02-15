794c8d7dfe52f813a843fdcf4b8a5ee8829a82cc
jascha <jascha@jaeh.at>
2020-02-15 20:28:35 +0100

    api/route: add some changing bits to response

--- example/api/v1/route.mjs
+++ example/api/v1/route.mjs
@@ -1,5 +1,6 @@
 export default (req, res) => {
-  const body = 'hello, world.'
+  const random = Math.floor(Math.random() * 1000)
+  const body = `hello, world. here is some pseudorandom: ${random}`
 
   return {
     code: 200,
