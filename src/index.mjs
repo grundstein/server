0ae0b571ce773719843bdcef6ab4cee51280b51e
jascha <jascha@jaeh.at>
2020-04-08 12:23:19 +0200

    use simplified cli arguments

--- src/index.mjs
+++ src/index.mjs
@@ -7,22 +7,21 @@ import defaultStore from '@grundstein/file-store'
 import { initStore, initApi } from './init/index.mjs'
 import { handler as defaultHandler } from './handler.mjs'
 
-export const runCluster = async (config = {}) => {
+export const run = async (config = {}) => {
   const startTime = log.hrtime()
 
   const {
     args = {},
     handler = defaultHandler,
     fileStore = defaultStore,
-    watcher = defaultWatcher,
   } = config
 
-  const { port = 8080, staticDir, apiDir, watch } = args
+  const { port = 8080, host = '127.0.0.1', dir = 'public' } = args
 
   try {
-    const store = await initStore(staticDir, fileStore)
+    const store = await initStore(dir, fileStore)
 
-    const api = await initApi(apiDir)
+    const api = await initApi(dir)
 
     const server = http.createServer(handler({ store, api }))
 
@@ -30,11 +29,11 @@ export const runCluster = async (config = {}) => {
       socket.end('HTTP/1.1 400 Bad Request\r\n\r\n')
     })
 
-    server.listen(port, () => {
+    server.listen(port, host, () => {
       const timeToListen = process.hrtime(startTime)
 
       log.success('Mainthread started', `pid: ${process.pid}`)
-      log(`server listening to localhost:${port}`)
+      log(`server listening to ${host}:${port}`)
 
       log.timeTaken(startTime, 'startup needed:')
     })
@@ -44,4 +43,4 @@ export const runCluster = async (config = {}) => {
   }
 }
 
-export default runCluster
+export default run
