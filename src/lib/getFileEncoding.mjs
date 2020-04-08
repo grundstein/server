3425b447a0b8b5d51cd982ebc9d28b2ae693b18e
jascha <jascha@jaeh.at>
2020-04-08 12:51:33 +0200

    lib/getFileEncoding: default is buffer again

--- src/lib/getFileEncoding.mjs
+++ src/lib/getFileEncoding.mjs
@@ -3,16 +3,14 @@ import is from '@magic/types'
 export const getFileEncoding = (file, acceptEncoding) => {
   const encoding = 'buffer'
 
-  if (is.empty(acceptEncoding) || !is.array(acceptEncoding)) {
-    return 'deflate'
-  }
-
-  if (acceptEncoding.includes('br') && file.br) {
-    encoding = 'br'
-  } else if (acceptEncoding.includes('gzip') && file.gzip) {
-    encoding = 'gzip'
-  } else if (acceptEncoding.includes('deflate') && file.deflate) {
-    encoding = 'deflate'
+  if (!is.empty(acceptEncoding) && is.array(acceptEncoding)) {
+    if (acceptEncoding.includes('br') && file.br) {
+      encoding = 'br'
+    } else if (acceptEncoding.includes('gzip') && file.gzip) {
+      encoding = 'gzip'
+    } else if (acceptEncoding.includes('deflate') && file.deflate) {
+      encoding = 'deflate'
+    }
   }
 
   return encoding
